0e2fc5a26e65f77a085d3d319675a8cb8f6565a4
pritesmistry <priteshmistry67@gmail.com>
2026-06-22 15:28:37 +0530

    Enhance Flask app with IVR functionality

--- app.py
+++ app.py
@@ -1,23 +1,63 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello"
+    return "Server running"
 
-@app.route("/answer", methods=["GET","POST"])
+
+@app.route("/answer", methods=["GET", "POST"])
 def answer():
 
     xml = """
 <Response>
-<Dial callerId="+912269988681">
+<GetInput action="https://plivo-ivr-qskz.onrender.com/ivr" method="POST" numDigits="1" timeout="10">
+    <Speak>Welcome to Bio Violet. Press 1 for sales. Press 2 for support.</Speak>
+</GetInput>
+
+<Speak>No input received. Goodbye.</Speak>
+</Response>
+"""
+
+    return xml, 200, {'Content-Type': 'text/xml'}
+
+
+@app.route("/ivr", methods=["GET", "POST"])
+def ivr():
+
+    digit = request.form.get("Digits")
+
+    print("Digit:", digit)
+
+    if digit == "1":
+
+        xml = """
+<Response>
+<Dial>
     <Number>+918087153266</Number>
 </Dial>
 </Response>
 """
 
+    elif digit == "2":
+
+        xml = """
+<Response>
+<Speak>Please email support at bioviolet dot in</Speak>
+</Response>
+"""
+
+    else:
+
+        xml = """
+<Response>
+<Speak>Invalid input</Speak>
+</Response>
+"""
+
     return xml, 200, {'Content-Type': 'text/xml'}
+
 
 if __name__ == "__main__":
     app.run()
