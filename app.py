aeb99af516cc65f9407e2748f55973ee00e7d2f6
pritesmistry <priteshmistry67@gmail.com>
2026-06-22 15:25:37 +0530

    Refactor app.py to simplify call handling

--- app.py
+++ app.py
@@ -1,70 +1,23 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
 
-
-# Home route
 @app.route("/")
 def home():
-    return "Hello - Server Working"
+    return "Hello"
 
-
-# Incoming call route
-@app.route("/answer", methods=["GET", "POST"])
+@app.route("/answer", methods=["GET","POST"])
 def answer():
-
-    print("CALL HIT /answer")
-    print("FORM DATA:", request.form)
 
     xml = """
 <Response>
-<GetInput action="https://plivo-ivr-qskz.onrender.com/ivr" method="POST" numDigits="1">
-    <Speak>Welcome to Bio Violet. Press 1 for sales. Press 2 for support.</Speak>
-</GetInput>
+<Dial callerId="+912269988681">
+    <Number>+918087153266</Number>
+</Dial>
 </Response>
 """
 
     return xml, 200, {'Content-Type': 'text/xml'}
-
-
-# After user presses a digit
-@app.route("/ivr", methods=["GET", "POST"])
-def ivr():
-
-    print("CALL HIT /ivr")
-    print("FORM DATA:", request.form)
-
-    digit = request.form.get("Digits")
-
-    print("DIGIT RECEIVED:", digit)
-
-    # Debug mode: just speak back what was pressed
-    if digit == "1":
-
-        xml = """
-<Response>
-<Speak>You pressed 1. Sales selected.</Speak>
-</Response>
-"""
-
-    elif digit == "2":
-
-        xml = """
-<Response>
-<Speak>You pressed 2. Please email support at bioviolet dot in.</Speak>
-</Response>
-"""
-
-    else:
-
-        xml = f"""
-<Response>
-<Speak>I did not receive a valid input. Received {digit}</Speak>
-</Response>
-"""
-
-    return xml, 200, {'Content-Type': 'text/xml'}
-
 
 if __name__ == "__main__":
     app.run()
