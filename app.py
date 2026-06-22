1171dee01b652b16bb37328ba9a9ea8ac05ff8cf
pritesmistry <priteshmistry67@gmail.com>
2026-06-22 14:55:19 +0530

    Add IVR functionality and XML responses

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,9 +6,52 @@ app = Flask(__name__)
 def home():
     return "Hello"
 
+
 @app.route("/answer", methods=["GET", "POST"])
 def answer():
-    return "PLIVO TEST WORKING"
+
+    xml = """
+<Response>
+<GetInput action="https://plivo-ivr-qskz.onrender.com/ivr" method="POST" numDigits="1">
+    <Speak>Welcome to Bio Leather. Press 1 for sales. Press 2 for support.</Speak>
+</GetInput>
+</Response>
+"""
+
+    return xml, 200, {'Content-Type': 'text/xml'}
+
+
+@app.route("/ivr", methods=["GET", "POST"])
+def ivr():
+
+    digit = request.form.get("Digits")
+
+    if digit == "1":
+
+        xml = """
+<Response>
+<Dial>+918087153266</Dial>
+</Response>
+"""
+
+    elif digit == "2":
+
+        xml = """
+<Response>
+<Speak>Please email support at bioleather dot in</Speak>
+</Response>
+"""
+
+    else:
+
+        xml = """
+<Response>
+<Speak>Invalid option</Speak>
+</Response>
+"""
+
+    return xml, 200, {'Content-Type': 'text/xml'}
+
 
 if __name__ == "__main__":
     app.run()
