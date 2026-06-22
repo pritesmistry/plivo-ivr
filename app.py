a3560f824f7a16615996b421cfc4e39728b977d0
pritesmistry <priteshmistry67@gmail.com>
2026-06-22 14:52:40 +0530

    Refactor app.py to simplify routes and responses

--- app.py
+++ app.py
@@ -1,53 +1,14 @@
-from flask import Flask, request
-from plivo import plivoxml
+from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Hello"
+
 @app.route("/answer", methods=["GET", "POST"])
 def answer():
-
-    response = plivoxml.ResponseElement()
-
-    get_input = plivoxml.GetInputElement(
-        action="https://plivo-ivr-qskz.onrender.com/ivr",
-        method="POST",
-        num_digits=1
-    )
-
-    get_input.add(
-        plivoxml.SpeakElement(
-            "Welcome to Bio Violet. Press 1 for sales. Press 2 for support."
-        )
-    )
-
-    response.add(get_input)
-
-    return str(response)
-
-
-@app.route("/ivr", methods=["GET", "POST"])
-def ivr():
-
-    digit = request.form.get("Digits")
-
-    response = plivoxml.ResponseElement()
-
-    if digit == "1":
-
-        dial = plivoxml.DialElement()
-        dial.addNumber("+918087153266")
-        response.add(dial)
-
-    elif digit == "2":
-
-        response.add(
-            plivoxml.SpeakElement(
-                "Please email support at bioleather dot in"
-            )
-        )
-
-    return str(response)
-
+    return "PLIVO TEST WORKING"
 
 if __name__ == "__main__":
     app.run()
